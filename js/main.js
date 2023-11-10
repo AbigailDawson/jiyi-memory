01605b2809e818d78febcbcf9f5b6d8665b5a5e6
Abigail Dawson <abigaildawson.dev@gmail.com>
2023-11-09 21:30:06 -0500

    declare Cards class to construct card objects and store in array

---
+++ js/main.js
@@ -0,0 +1,59 @@
+/*----- constants -----*/
+
+
+/*----- state variables -----*/
+
+// Create an array of instances of a class: https://stackoverflow.com/questions/52377344/javascript-array-of-instances-of-a-class
+
+class Card {
+    constructor(char, meaning, flipped) {
+        this.char = char;
+        this.meaning = meaning;
+        this.flipped = flipped;
+    }
+}
+
+class Cards {
+    constructor() {
+        this.cards = [];
+    }
+    newCard(char, meaning, flipped) {
+        let card = new Card(char, meaning, flipped);
+        this.cards.push(card);
+        return card;
+    }
+    get allCards() {
+        return this.cards;
+    }
+}
+
+let deck = new Cards(); // deck.cards is the array
+deck.newCard('愛', 'love', false);
+deck.newCard('書', 'book', false);
+deck.newCard('國', 'country', false);
+deck.newCard('家', 'family', false);
+deck.newCard('學', 'learn', false);
+deck.newCard('吃', 'eat', false);
+deck.newCard('喝', 'drink', false);
+deck.newCard('貓', 'cat', false);
+deck.newCard('狗', 'dog', false);
+deck.newCard('夢', 'dream', false);
+
+board = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
+]
+
+let rndIdx = deck.cards[Math.floor(Math.random() * deck.cards.length)];
+
+board.forEach((column, idx) => {
+    column.forEach((value, idx) => {
+        value = rndIdx;
+    });
+});
+
+/*----- cached elements -----*/
+/*----- event listeners -----*/
+/*----- functions -----*/
