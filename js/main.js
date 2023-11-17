dcfa57ed9d42605a766923a2b6546ff739ed3087
Abigail Dawson <abigaildawson.dev@gmail.com>
2023-11-16 19:51:21 -0500

    Fix issue with font sizing

--- js/main.js
+++ js/main.js
@@ -220,7 +220,12 @@ function renderBoard() {
                 cellEl.innerText = '';
 
             } else if (card.matched !== true && card.flipped === true) {
-                if (card.text.match(/[\u4E00-\u9FFF]/)) cellEl.setAttribute('lang', 'zh-Hant');
+                if (card.text.match(/[\u4E00-\u9FFF]/)) {
+                    cellEl.setAttribute('lang', 'zh-Hant');
+                } else {
+                    cellEl.fontFamily = '\'Paytone One\', sans-serif';
+                    cellEl.fontSize = '2.3vmin';
+                }
                 cellEl.style.backgroundColor = 'var(--flipped-card-color)';
                 cellEl.innerText = card.text;
             }
